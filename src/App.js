55d33fc1e22c03e71b59c3abaa0e1e5a285402b0
asumpta <sumgathoni@gmail.com>
2023-11-06 09:46:36 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      {/* header */}
+      <Header />
+      {/* app body */}
+      <div className="app__body">
+        {/* sidebar*/}
+        <SideBar />
+        {/* feed */}
+        {/* widget */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          alt=""
+          src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Linkedin_Shiny_Icon.svg"
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption title="Home" Icon={HomeIcon} />
+        <HeaderOption title="My NetWork" Icon={SupervisorAccountIcon} />
+        <HeaderOption title="Jobs" Icon={BusinessCenterIcon} />
+        <HeaderOption title="Messaging" Icon={ChatIcon} />
+        <HeaderOption title="Notifications" Icon={NotificationsIcon} />
+        <HeaderOption
+          avatar="https://upload.wikimedia.org/wikipedia/commons/0/09/Cheerful_Smile_Emoji.png"
+          title="me"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
