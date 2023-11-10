3b85824afc8e8485912a599af690bd8534b4c4dc
asumpta <sumgathoni@gmail.com>
2023-11-10 15:22:46 +0300

    intergrated firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import CreateIcon from "@mui/icons-material/Create";
 import InputOption from "./InputOption";
@@ -7,12 +7,36 @@ import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 import Post from "./Post";
+import { db } from "./firebase.js";
+import firebase from "firebase";
 
 function Feed() {
+  const [input, setInput] = useState("");
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  }, []);
 
   function sendPost(e) {
     e.preventDefault();
+    db.collection("posts").add({
+      name: "Asumpta",
+      description: "Test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput("");
   }
 
   return (
@@ -21,7 +45,11 @@ function Feed() {
         <div className="feed__input">
           <CreateIcon />
           <form>
-            <input type="text" />
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
             <button onClick={sendPost} type="submit">
               Send
             </button>
@@ -39,12 +67,13 @@ function Feed() {
         </div>
       </div>
       {/* posts */}
-      {posts.map((post) => (
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
         <Post
-          name="Asumpta Gathoni"
-          description="Description"
-          message="Message goes here"
-          photoUrl={""}
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
         />
       ))}
       <Post
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,9 @@
+import firebase from "firebase";
+
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBwlPit3KejkBMFbhUDbJ3vRCxK2dohfJU",
   authDomain: "linkedin-clone-6be30.firebaseapp.com",
@@ -6,3 +12,9 @@ const firebaseConfig = {
   messagingSenderId: "159014026308",
   appId: "1:159014026308:web:bf5abc7eda9d77a9f3d011",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
