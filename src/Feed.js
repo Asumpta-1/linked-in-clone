551b9e1cebd8b907233e063ea928055ddbdc10ce
asumpta <sumgathoni@gmail.com>
2023-11-10 12:40:12 +0300

    building the feed posts

--- src/Feed.js
+++ src/Feed.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Feed.css";
 import CreateIcon from "@mui/icons-material/Create";
+import InputOption from "./InputOption";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import Post from "./Post";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  function sendPost(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -10,10 +22,37 @@ function Feed() {
           <CreateIcon />
           <form>
             <input type="text" />
-            <button type="submit">Send</button>
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
+        <div className="feed__inputOption">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
+        </div>
       </div>
+      {/* posts */}
+      {posts.map((post) => (
+        <Post
+          name="Asumpta Gathoni"
+          description="Description"
+          message="Message goes here"
+          photoUrl={""}
+        />
+      ))}
+      <Post
+        name="Asumpta Gathoni"
+        description="Description"
+        message="Message goes here"
+        photoUrl={""}
+      />
     </div>
   );
 }
---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwlPit3KejkBMFbhUDbJ3vRCxK2dohfJU",
+  authDomain: "linkedin-clone-6be30.firebaseapp.com",
+  projectId: "linkedin-clone-6be30",
+  storageBucket: "linkedin-clone-6be30.appspot.com",
+  messagingSenderId: "159014026308",
+  appId: "1:159014026308:web:bf5abc7eda9d77a9f3d011",
+};
